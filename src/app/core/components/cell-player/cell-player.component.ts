76685e9dd3799afcc2e7baf4ff516f7a801c3776
Jose Vega <josevega234@gmail.com>
2020-12-15 20:21:35 +0100

    bugfix

--- src/app/core/components/cell-player/cell-player.component.ts
+++ src/app/core/components/cell-player/cell-player.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
+import * as WaveSurfer from 'wavesurfer.js';
 
 @Component({
   selector: 'app-cell-player',
   templateUrl: './cell-player.component.html',
   styleUrls: ['./cell-player.component.scss']
 })
-export class CellPlayerComponent implements OnInit {
+export class CellPlayerComponent implements OnInit, AfterViewInit {
+
+  @Input() filePath: string;
+  @Input() id: string;
+  ws: WaveSurfer;
+  playerId: string;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.playerId = 't-' + this.id.split('-')[0];
+  }
+
+  ngAfterViewInit(): void {
+    this.ws = WaveSurfer.create({
+      container: '#' + this.playerId,
+      height: 19,
+      waveColor: '#c68760',
+      progressColor: '#03a9f4',
+      // barWidth: 1,
+      // barHeight: 2, // the height of the wave
+      // barGap: null
+    });
+
+    this.ws.load(this.filePath);
+
+    this.ws.on('seek', () => { this.ws.playPause(); });
   }
 
 }
--- src/app/core/models/MusicTag.ts
+++ src/app/core/models/MusicTag.ts
@@ -1,5 +1,8 @@
 import { ArtImage } from './ArtImage';
+import { v4 } from 'uuid';
+
 export class MusicTag {
+  id: string;
   fileIndex: number;
   titleTag: string;
   artistTag: string;
@@ -13,8 +16,18 @@ export class MusicTag {
   filename: string;
   filepath: string;
 
-
-  constructor(fileIndex: number, titleTag: string, artistTag: string, albumTag: string, genreTag: string, yearTag: number, bpmTag: number, keyTag: string, imageTag: ArtImage, filename: string, filepath: string) {
+  constructor(
+    fileIndex: number,
+    titleTag: string,
+    artistTag: string,
+    albumTag: string,
+    genreTag: string,
+    yearTag: number,
+    bpmTag: number,
+    keyTag: string,
+    imageTag: ArtImage,
+    filename: string,
+    filepath: string) {
     this.fileIndex = fileIndex;
     this.titleTag = titleTag;
     this.artistTag = artistTag;
@@ -24,8 +37,9 @@ export class MusicTag {
     this.bpmTag = bpmTag;
     this.keyTag = keyTag;
     this.imageTag = imageTag;
-    this.hasCover = this.imageTag !== undefined
+    this.hasCover = this.imageTag !== undefined;
     this.filename = filename;
     this.filepath = filepath;
+    this.id = v4();
   }
 }
