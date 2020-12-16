1a17ad55b38968229ec9b200f9270cdf4d7a074e
Jose Vega <josevega234@gmail.com>
2020-12-16 11:08:31 +0100

    Bug

--- src/app/core/components/cell-player/cell-player.component.ts
+++ src/app/core/components/cell-player/cell-player.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, DoCheck, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import * as WaveSurfer from 'wavesurfer.js';
 
 @Component({
@@ -6,33 +6,40 @@ import * as WaveSurfer from 'wavesurfer.js';
   templateUrl: './cell-player.component.html',
   styleUrls: ['./cell-player.component.scss']
 })
-export class CellPlayerComponent implements OnInit, AfterViewInit {
+export class CellPlayerComponent implements OnInit, AfterViewInit, OnChanges, DoCheck {
 
   @Input() filePath: string;
   @Input() id: string;
   ws: WaveSurfer;
   playerId: string;
+  path: string;
 
   constructor() { }
 
   ngOnInit(): void {
-    this.playerId = 't-' + this.id.split('-')[0];
   }
 
   ngAfterViewInit(): void {
+    console.log(`afterview ${this.id}`);
     this.ws = WaveSurfer.create({
       container: '#' + this.playerId,
       height: 19,
       waveColor: '#c68760',
       progressColor: '#03a9f4',
-      // barWidth: 1,
-      // barHeight: 2, // the height of the wave
-      // barGap: null
+      barWidth: 1,
+      barHeight: 2, // the height of the wave
+      barGap: null
     });
-
-    this.ws.load(this.filePath);
-
+    this.ws.load(this.path);
     this.ws.on('seek', () => { this.ws.playPause(); });
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    this.playerId = 't-' + changes.id.currentValue.split('-')[0];
+    this.path = changes.filePath.currentValue;
+    if (changes.filePath.isFirstChange()) { return; }
+    this.ws.load(this.path);
+    console.log(`ngChanges ${this.playerId}`);
+    console.log(changes);
+  }
 }
