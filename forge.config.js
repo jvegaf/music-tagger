9f14c36f27368ce4b6d832f94c760cfe1cbbff14
Jose Vega <josevega234@gmail.com>
2022-03-05 15:15:52 +0100

    refactor package json
    Signed-off-by: Jose Vega <josevega234@gmail.com>

--- forge.config.js
+++ forge.config.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 const appIcon = () => {
   switch (process.platform) {
-    case 'win32': return path.join('src', 'assets', 'icons','win','icon.ico');
-    case 'linux': return path.join('src', 'assets', 'icons','png','1024x1024.png');
-    case 'darwin': return path.join('src', 'assets', 'icons','mac','icon.icns');
+    case 'win32': return path.join('src', 'renderer', 'assets', 'icons','win','icon.ico');
+    case 'linux': return path.join('src', 'renderer','assets', 'icons','png','1024x1024.png');
+    case 'darwin': return path.join('src', 'renderer','assets', 'icons','mac','icon.icns');
   }
 }
 
@@ -18,8 +18,8 @@ module.exports = {
       'name': '@electron-forge/maker-squirrel',
       'config': {
         'name': 'music_tagger',
-        'loadingGif': path.join('src', 'assets', 'splash-screen.gif'),
-        'setupIcon': path.join('src', 'assets', 'icons','win','icon.ico'),
+        'loadingGif': path.join('src', 'renderer', 'assets', 'splash-screen.gif'),
+        'setupIcon': path.join('src', 'renderer', 'assets', 'icons','win','icon.ico'),
         'skipUpdateIcon': true
       }
     },
@@ -35,7 +35,7 @@ module.exports = {
         'options': {
           'maintainer': 'Jose Vega',
           'homepage': 'https://github.com/jvegaf/music-tagger',
-          'icon': 'src/assets/icons/mac/icon.icns',
+          'icon': 'src/renderer/assets/icons/mac/icon.icns',
           'categories': [
             'AudioVideo',
             'Audio',
@@ -50,7 +50,7 @@ module.exports = {
         'options': {
           'maintainer': 'Jose Vega',
           'homepage': 'https://github.com/jvegaf/music-tagger',
-          'icon': 'src/assets/icons/mac/icon.icns',
+          'icon': 'src/renderer/assets/icons/mac/icon.icns',
           'categories': [
             'AudioVideo',
             'Audio',
